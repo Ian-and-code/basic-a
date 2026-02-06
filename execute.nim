c46e062996d34f22fa6bf9150f91b8389b72235f
Ian-and-code <supergachaworld4@gmail.com>
2026-02-05 22:57:53 -0300

    last commit in local pc

--- execute.nim
+++ execute.nim
@@ -3,13 +3,23 @@ import strutils, tables, math, re, os, times
 
 const nan = 0.0 / 0.0
 
+template writeError[T...](args: T...): return type =
+  for i in args:
+    stderr.write(i)
+  stderr.write("\n")
+  quit(1)
+
+proc checkArgsLen(expected: int, realArgc: int, op: string) =
+  if expected != realArgc:
+    writeError("ArgumentNumberError: op ", op, " expected ", expected, " but recived ", realArgc)
+
 proc isIdent(s: string) =
   if s.len == 0:
-    stderr.write("Error: identificador vacío\n")
+    writeError("Error: identificador vacío\n")
     return
   for i, c in s:
     if not (('a' <= c and c <= 'z') or ('A' <= c and c <= 'Z') or c == '_' or (i != 0 and '0' <= c and c <= '9')):
-      stderr.write("Error: identificador inválido: ", s, "\n")
+      writeError("Error: identificador inválido: ", s, "\n")
       break
 
 
@@ -55,10 +65,14 @@ func cutAfterLast(o: string, char: char; includeChar: bool = false): string =
 proc execute*(code: string, inFile: bool = false) =
   var vars = initTable[string, tuple[value: string, `type`: string]]()
   var labels = initTable[string, int]()
-  vars["debug"] = (value: "0", `type`: "NUMBER")
+  var funcs = initTable[string, int]()
+  vars["%%n"] = (value: "\n", `type`: "STRING")
+  vars["%%t"] = (value: "\t", `type`: "STRING")
+  vars["%%b"] = (value: "\b", `type`: "STRING")
+  vars["%%r"] = (value: "\r", `type`: "STRING")
   let lines = code.strip().splitLines()
   var index = 0
-
+  var callStack: seq[int] = @[]
   # helpers
   func getV(o: string): string =
     if vars.hasKey(o):
@@ -96,8 +110,25 @@ proc execute*(code: string, inFile: bool = false) =
     elif isInt(o):
       return o.getI()
     else:
-      stderr.write("Error: label no encontrado: ", o, "\n")
+      writeError("Error: label no encontrado: ", o, "\n")
       return -1
+
+  while index  < lines.len:
+    if isBlankOrSemicolon(lines[index]):
+      index += 1
+      continue
+    let li = parseLine(lines[index].strip().cutAfterLast(';'))
+    case li.op:
+    of ".":
+      isIdent(li.argv[0])
+      labels[li.argv[0]] = index
+    of ":":
+      isIdent(li.argv[0])
+      funcs[li.argv[0]] = index
+
+    index += 1
+
+  index = 0
 
   while index < lines.len:
     if isBlankOrSemicolon(lines[index]):
@@ -105,116 +136,149 @@ proc execute*(code: string, inFile: bool = false) =
       continue
     let li = parseLine(lines[index].strip().cutAfterLast(';'))
     case li.op
+    of "CALL":
+      checkArgsLen(1, li.argc, "CALL")
+      callStack.add(index+1)
+      index = funcs[li.argv[0]]
+    of "RETURN":
+      checkArgsLen(0, li.argc, "RETURN")
+      index = callStack.pop()
+      continue
     of "COPYT":
+      checkArgsLen(2, li.argc, "COPYT")
       vars[li.argv[0]] = (value: li.argv[1].getV().getT(), `type`: "TYPE")
     of "COPYV":
+      checkArgsLen(2, li.argc, "COPYV")
       try:
         vars[li.argv[0]].value = li.argv[1].getV()
       except:
         vars[li.argv[0]] = (value: li.argv[1].getV(), `type`: vars[li.argv[1]].`type`)
     of "SETA":
+      checkArgsLen(2, li.argc, "SETA")
       isIdent(li.argv[0])
       if vars.hasKey(li.argv[0]):
         vars[li.argv[0]].value = li.argv[1]   # solo asigna
       else:
         vars[li.argv[0]] = (value: li.argv[1], `type`: li.argv[1].getT())
     of "SET":
+      checkArgsLen(2, li.argc, "SET")
       if vars.hasKey(li.argv[0]):
         vars[li.argv[0]].value = li.argv[1]   # solo asigna
       else:
-        stderr.write("Error: SET de variable no declarada: ", li.argv[0], "\n")
+        writeError("Error: SET de variable no declarada: ", li.argv[0], "\n")
     of "SETL":
+      checkArgsLen(2, li.argc, "SETL")
       isIdent(li.argv[0])
       labels[li.argv[0]] = li.argv[1].getI()
     of "SETN":
+      checkArgsLen(2, li.argc, "SETN")
       isIdent(li.argv[0])
       vars[li.argv[0]] = (value: $(li.argv[1].getF()), `type`: "NUMBER")
     of "SETS":
+      checkArgsLen(2, li.argc, "SET")
       isIdent(li.argv[0])
       vars[li.argv[0]] = (value: li.argv[1], `type`: "STRING")
     of "SETB":
+      checkArgsLen(2, li.argc, "SETB")
       isIdent(li.argv[0])
       vars[li.argv[0]] = (value: $(if li.argv[1].getB(): "TRUE" else: "FALSE"), `type`: "BOOL")
     of "RESET":
+      checkArgsLen(2, li.argc, "RESET")
       if not vars.hasKey(li.argv[0]):
-        stderr.write("Error: SET de variable no declarada: ", li.argv[0], "\n")
+        writeError("Error: SET de variable no declarada: ", li.argv[0], "\n")
       else:
         vars[li.argv[0]] = (value: li.argv[1], `type`: li.argv[1].getT())
     # operaciones solo si tipos compatibles
     of "ADD":
+      checkArgsLen(2, li.argc, "ADD")
       if getT(li.argv[0]) == "NUMBER" and getT(li.argv[1]) == "NUMBER":
         vars[li.argv[0]].value = $((li.argv[0].getF()) + (li.argv[1].getF()))
       else:
-        stderr.write("Error: tipos incompatibles en ADD\n")
+        writeError("Error: tipos incompatibles en ADD\n")
     of "SUB":
+      checkArgsLen(2, li.argc, "SUB")
       if getT(li.argv[0]) == "NUMBER" and getT(li.argv[1]) == "NUMBER":
         vars[li.argv[0]].value = $((li.argv[0].getF()) - (li.argv[1].getF()))
       else:
-        stderr.write("Error: tipos incompatibles en SUB\n")
+        writeError("Error: tipos incompatibles en SUB\n")
     of "MUL":
+      checkArgsLen(2, li.argc, "MUL")
       if getT(li.argv[0]) == "NUMBER" and getT(li.argv[1]) == "NUMBER":
         vars[li.argv[0]].value = $((li.argv[0].getF()) * (li.argv[1].getF()))
       else:
-        stderr.write("Error: tipos incompatibles en MUL\n")
+        writeError("Error: tipos incompatibles en MUL\n")
     of "DIV":
+      checkArgsLen(2, li.argc, "DIV")
       if getT(li.argv[0]) == "NUMBER" and getT(li.argv[1]) == "NUMBER":
         vars[li.argv[0]].value = $((li.argv[0].getF()) / (li.argv[1].getF()))
       else:
-        stderr.write("Error: tipos incompatibles en DIV\n")
+        writeError("Error: tipos incompatibles en DIV\n")
     of "PRINT":
-      stdout.write(li.argv[0].getV())
+      var prints = ""
+      for i, v in li.argv:
+        prints &= v.getV()
+      stdout.write(prints)
     of "PRINTLN":
-      stdout.write(li.argv[0].getV(), "\n")
+      var prints = ""
+      for i, v in li.argv:
+        prints &= v.getV()
+      stdout.write(prints, "\n")
     of "JMP":
+      checkArgsLen(1, li.argc, "JMP")
       index = getL(li.argv[0])
       continue
     of "JZ":
+      checkArgsLen(2, li.argc, "JZ")
       if li.argv[0].getV().getT() != "NUMBER":
-        stderr.write("Error: tipo incompatibles en JZ\n")
+        writeError("Error: tipo incompatibles en JZ\n")
       elif li.argv[0].getV().getF() == 0:
         index = getL(li.argv[1])
         continue
     of "JNZ":
+      checkArgsLen(2, li.argc, "JNZ")
       if li.argv[0].getV().getT() != "NUMBER":
-        stderr.write("Error: tipo incompatibles en JNZ\n")
+        writeError("Error: tipo incompatibles en JNZ\n")
       elif li.argv[0].getV().getF() != 0:
         index = getL(li.argv[1])
         continue
     of "JE":
+      checkArgsLen(3, li.argc, "JE")
       if li.argv[0].getV() == li.argv[1].getV():
         index = getL(li.argv[2])
         continue
     of "JNE":
+      checkArgsLen(3, li.argc, "JNE")
       if li.argv[0].getV() != li.argv[1].getV():
         index = getL(li.argv[2])
         continue
     of "JLT":
+      checkArgsLen(3, li.argc, "JLT")
       if li.argv[0].getV().getT() != "NUMBER" or li.argv[1].getV().getT() != "NUMBER":
-        stderr.write("Error: tipos incompatibles en JLT\n")
+        writeError("Error: tipos incompatibles en JLT\n")
       if li.argv[0].getV().getF() < li.argv[1].getV().getF():
         index = getL(li.argv[2])
         continue
     of "JGT":
+      checkArgsLen(3, li.argc, "JGT")
       if li.argv[0].getV().getT() != "NUMBER" or li.argv[1].getV().getT() != "NUMBER":
-        stderr.write("Error: tipos incompatibles en JGT\n")
+        writeError("Error: tipos incompatibles en JGT\n")
       if li.argv[0].getV().getF() > li.argv[1].getV().getF():
         index = getL(li.argv[2])
         continue
-    of ".":
-      isIdent(li.argv[0])
-      labels[li.argv[0]] = index
     of "END":
+      checkArgsLen(0, li.argc, "END")
       return
     of "NOP":
       discard
     of "SLEEP":
+      checkArgsLen(1, li.argc, "SLEEP")
       sleep(li.argv[0].getV().getI())
     else:
-      if li.op.strip().isBlankOrSemicolon():
+      if li.op.strip().isBlankOrSemicolon() or li.op == "." or li.op == ":":
         index+=1
         continue
-      stderr.write("operación " & li.op & " no es valida\n")
+      writeError("operación " & li.op & " no es valida\n")
 
     index += 1
 
-  if inFile:stderr.write("Error: operación end no ejecutada\n")
+  if inFile:writeError("Error: operación end no ejecutada\n")
